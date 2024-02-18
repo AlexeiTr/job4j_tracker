b1bc0abb38c62c243194c83bb0f96f4037b784c0
Rasp <mcarisma2000@yandex.ru>
2024-02-18 10:58:20 +0300

    6. Правила Checkstyle [#504981 #481801]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,23 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+    private final int size = 10;
+    private String name;
+    private String surname;
+    private static final String NEW_VALUE = "";
+
+    public Broken() { }
+
+    public void echo() { }
+
+    public void method(int a, int b, int c, int d, int e, int r, int g) {
+
+    }
+
+    public void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+}
