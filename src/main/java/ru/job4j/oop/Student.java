cf373a014b969029ff28c34d1bf01cafbf8f994c
Rasp <mcarisma2000@yandex.ru>
2024-02-21 00:31:29 +0300

    1.2. Вызов метода объекта [#166085 #481930]

--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -13,6 +13,10 @@ public class Student {
     public static void main(String[] args) {
         Student petya = new Student();
         petya.music();
+        petya.music();
+        petya.music();
+        petya.sing();
+        petya.sing();
         petya.sing();
     }
 }
